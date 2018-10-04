adab4576031c4bf7a9c7b01fa9d23748232da2e0
Amber Moreno <amber.e.moreno@gmail.com>
2018-10-04 16:10:56 -0600

    user tests passing

--- TravelAppJPA/src/test/java/com/skilldistillery/travelapp/tests/UserTests.java
+++ TravelAppJPA/src/test/java/com/skilldistillery/travelapp/tests/UserTests.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.travelapp.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -31,14 +36,37 @@ public class UserTests {
 
 	@Test
 	public void test_user_mappings() {
+		assertEquals("JamesG", user.getName());
+		assertEquals("password", user.getPassword());
+		assertEquals("JG@wander.com", user.getEmail());
+		assertEquals("standard", user.getRole());
+		assertEquals(true, user.isActive());
 	}
 
 	@Test
 	public void test_user_followers_mappings() {
+		assertNotEquals(0, user.getFollowers().size());
 	}
 
 	@Test
 	public void test_user_profile_mappings() {
+		assertEquals("James", user.getProfile().getFirstName());
+		assertEquals("Gill", user.getProfile().getLastName());
+		assertEquals("images/UserPics/James_Gill.png", user.getProfile().getImgLink());
+	}
+	
+	@Test
+	public void test_user_addFollower_method() {
+		User u = user.getFollowers().get(0);
+		user.addFollower(u);
+		assertTrue(user.getFollowers().contains(u));
+	}
+	
+	@Test
+	public void test_user_removeFollower_method() {
+		User u = user.getFollowers().get(0);
+		user.removeFollower(u);
+		assertFalse(user.getFollowers().contains(u));
 	}
 
 }
