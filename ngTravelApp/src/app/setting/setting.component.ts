3d971e02a7ec0ac4659d1c1f1a6bb1065cc6f4ca
Isaac <Isaacsunoo@gmail.com>
2018-10-05 13:34:36 -0600

    functioning registration

--- ngTravelApp/src/app/setting/setting.component.ts
+++ ngTravelApp/src/app/setting/setting.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Registration } from '../models/registration';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
   styleUrls: ['./setting.component.css']
 })
+
 export class SettingComponent implements OnInit {
 
+  selected: Registration = null;
+  editProfile: Registration = new Registration();
 
+  setEditProfile = function () {
+    this.edit = Object.assign({}, this.selected);
+
+};
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
