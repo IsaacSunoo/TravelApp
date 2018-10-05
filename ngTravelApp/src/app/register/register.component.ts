836667e94ddaad0329bf81db21f22736619d8516
Amber Moreno <amber.e.moreno@gmail.com>
2018-10-05 09:58:45 -0600

    register.component.ts

--- ngTravelApp/src/app/register/register.component.ts
+++ ngTravelApp/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: User = new User();
+  registerUser = function(form: NgForm) {
+    const user = form.value;
+    console.log(user);
 
-  constructor() { }
+    this.userService.register(user).subscribe(
+      data => {
+        console.log('returned data: ');
+        console.log(data);
+        // this.router.navigateByUrl('trips');
+      },
+      err => {
+        // this.router.navigateByUrl('register');
+      }
+    );
+  };
+
+  constructor(private userService: UserService, private router: ActivatedRoute) { }
 
   ngOnInit() {
   }
