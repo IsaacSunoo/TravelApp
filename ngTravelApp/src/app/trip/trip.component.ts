25e40d35f6d12d0e4cdd9d307476d85eb7af7d9d
Amber Moreno <amber.e.moreno@gmail.com>
2018-10-06 16:34:55 -0600

    index and create routes for trip

--- ngTravelApp/src/app/trip/trip.component.ts
+++ ngTravelApp/src/app/trip/trip.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from '../models/trip';
+import { NgForm } from '@angular/forms';
+import { TripService } from '../trip.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-trip',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trip.component.css']
 })
 export class TripComponent implements OnInit {
+  trips: Trip[] = [];
+  newTrip: Trip = new Trip();
+  id;
 
-  constructor() { }
+  createTrip(form: NgForm) {
+    const trip = form.value;
+    console.log(trip);
+
+    this.tripService.create(trip).subscribe(
+      data => {
+        console.log('returned data: ');
+        console.log(data);
+      }
+    );
+  }
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private tripService: TripService) { }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    // this.loadTrip(this.id);
+    //   console.log(this.id);
   }
 
 }
+
