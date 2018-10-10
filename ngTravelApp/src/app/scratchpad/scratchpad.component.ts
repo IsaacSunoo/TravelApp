fcc1c9c3f65f42a222e4149453295704a540dc76
Richard Paleczny <palecznyr@gmail.com>
2018-10-10 13:53:04 -0600

    andrew test

---
+++ ngTravelApp/src/app/scratchpad/scratchpad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scratchpad',
+  templateUrl: './scratchpad.component.html',
+  styleUrls: ['./scratchpad.component.css']
+})
+export class ScratchpadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
