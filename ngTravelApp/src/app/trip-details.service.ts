cd00d1f1697c04044952cac285d57527bd111909
Isaac <Isaacsunoo@gmail.com>
2018-10-10 14:50:47 -0600

    update

--- ngTravelApp/src/app/trip-details.service.ts
+++ ngTravelApp/src/app/trip-details.service.ts
@@ -3,6 +3,7 @@ import { Observable, throwError } from 'rxjs';
 import { UpdateProfile } from './models/update-profile';
 import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { Posts } from './models/posts';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,9 @@ import { HttpClient } from '@angular/common/http';
 export class TripDetailsService {
   private baseUrl = 'http://localhost:8080/';
   private url = this.baseUrl + 'api/profiles';
+  private postUrl = this.baseUrl + 'api/posts';
 
-  public show(id: string): Observable<UpdateProfile> {
+  public showProfile(id: string): Observable<UpdateProfile> {
     return this.http.get<UpdateProfile>(this.url + '/' + id).pipe(
       catchError((err: any) => {
         console.log(err);
@@ -22,5 +24,25 @@ export class TripDetailsService {
     );
   }
 
-  constructor(private http: HttpClient) { }
+  public showTripDetails(id: string): Observable<UpdateProfile> {
+    return this.http.get<UpdateProfile>(this.postUrl + '/' + id).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        console.log('Error in service');
+
+        return throwError('Error retrieving Trip: ' + ' Status: ' + err.status);
+      })
+    );
+  }
+
+  public index(id): Observable<Posts[]> {
+    return this.http.get<Posts[]>(this.postUrl + '/' + id).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('Error retrieving Trip: ' + ' Status: ' + err.status);
+      })
+    );
+  }
+
+  constructor(private http: HttpClient) {}
 }
