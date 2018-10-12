8cda93f7697c92c1c7103d0c14bdf3f5e70d9e9e
Amber Moreno <amber.e.moreno@gmail.com>
2018-10-12 16:47:36 -0600

    deployment changes

--- TravelAppREST/src/main/java/com/skilldistillery/travelapp/TravelAppRestApplication.java
+++ TravelAppREST/src/main/java/com/skilldistillery/travelapp/TravelAppRestApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.travelapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelAppRestApplication {
+public class TravelAppRestApplication extends SpringBootServletInitializer {
+	
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(TravelAppRestApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelAppRestApplication.class, args);
