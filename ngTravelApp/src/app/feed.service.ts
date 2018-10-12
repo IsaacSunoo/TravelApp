ad0018d95fe7c00e8a4db38c4e147739c67a3d24
Amber Moreno <amber.e.moreno@gmail.com>
2018-10-12 15:20:22 -0600

    fixed search

--- ngTravelApp/src/app/feed.service.ts
+++ ngTravelApp/src/app/feed.service.ts
@@ -1,4 +1,4 @@
-import { environment } from './../environments/environment.prod';
+import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
 import { UpdateProfile } from './models/update-profile';
 import { catchError } from 'rxjs/operators';
