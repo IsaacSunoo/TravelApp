0504e18fe96cc55e6e50c8800d929922da53c9d2
Richard Paleczny <palecznyr@gmail.com>
2018-10-11 21:48:38 -0600

    update

--- ngTravelApp/src/app/profile.service.ts
+++ ngTravelApp/src/app/profile.service.ts
@@ -4,12 +4,12 @@ import { UpdateProfile } from './models/update-profile';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { User } from './models/user';
+import { Trip } from './models/trip';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-
   private baseUrl = 'http://localhost:8080/';
   private url = this.baseUrl + 'api/profiles';
 
@@ -19,7 +19,9 @@ export class ProfileService {
         console.log(err);
         console.log('Error in service');
 
-        return throwError('Error retrieving Profile: ' + ' Status: ' + err.status);
+        return throwError(
+          'Error retrieving Profile: ' + ' Status: ' + err.status
+        );
       })
     );
   }
@@ -34,33 +36,44 @@ export class ProfileService {
 
   // following a user is adding a follower to followed person's followers
   public followUser(id: string, testId: string) {
-    return this.http.post<any>(this.baseUrl + 'addfollower/' + id + '/' + testId, {}).pipe(
+    return this.http
+      .post<any>(this.baseUrl + 'addfollower/' + id + '/' + testId, {})
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('Error retrieving data ');
+        })
+      );
+  }
+  // unfollow a user that you are following
+  public unfollowUser(id: string, testId: string) {
+    return this.http
+      .delete(this.baseUrl + 'removefollower/' + id + '/' + testId)
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('Error retrieving data ' + err.status);
+        })
+      );
+  }
+
+  public followingIndex(id: string): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'findfollowers/' + id).pipe(
       catchError((err: any) => {
         console.log(err);
-        return throwError('Error retrieving data ');
+        return throwError('Error retrieving data ' + err.status);
       })
     );
-    }
-  // unfollow a user that you are following
-    public unfollowUser(id: string, testId: string) {
-      return this.http.delete(this.baseUrl + 'removefollower/' + id + '/' + testId).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError('Error retrieving data ' + err.status);
-        })
-      );
-    }
+  }
 
-    public followingIndex(id: string): Observable<User[]> {
-      return this.http.get<User[]>(this.baseUrl + 'findfollowers/' + id).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError('Error retrieving data ' + err.status);
-        })
-      );
-    }
-
+  public getFavoriteTripsByProfileId(id: string): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.url}/${id}/favoriteTrips`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('Error retrieving data ' + err.status);
+      })
+    );
+  }
 
   constructor(private http: HttpClient) {}
 }
-
